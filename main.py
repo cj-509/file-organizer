6d86b10659162f4f394520daccf38c7285c0683e
cj <cj@DESKTOP-M2KOFHD.hitronhub.home>
2024-01-05 11:43:14 -0700

    .
    added function to check file type

--- main.py
+++ main.py
@@ -1,6 +1,48 @@
 import os
 #======== a script that manage files
 
+
+audio = (".3ga", ".aac", ".ac3", ".aif", ".aiff",
+         ".alac", ".amr", ".ape", ".au", ".dss",
+         ".flac", ".flv", ".m4a", ".m4b", ".m4p",
+         ".mp3", ".mpga", ".ogg", ".oga", ".mogg",
+         ".opus", ".qcp", ".tta", ".voc", ".wav",
+         ".wma", ".wv")
+
+video = (".webm", ".MTS", ".M2TS", ".TS", ".mov",
+         ".mp4", ".m4p", ".m4v", ".mxf")
+
+img = (".jpg", ".jpeg", ".jfif", ".pjpeg", ".pjp", ".png",
+       ".gif", ".webp", ".svg", ".apng", ".avif")
+
+doc = (".epub", ".mobi", ".pdf", ".azw", ".txt", 
+       ".html", ".htm",".fb2", ".cbz", ".cbr", 
+       ".lit", ".iba")  #document files
+
+data = (".csv", ".xlsx", ".xls", ".json", ".xml", 
+        ".sql", ".tsv", ".h5", ".hdf5", ".yaml", 
+        ".yml", ".feather", ".parquet", ".avro", 
+        ".sqlite", ".db")
+
+def is_audio(file):
+    return os.path.splitext(file)[1] in audio
+
+def is_video(file):
+    return os.path.splitext(file)[1] in video
+
+def is_image(file):
+    return os.path.splitext(file)[1] in img
+
+def is_screenshot(file):
+    name, ext = os.path.splitext(file)
+    return (ext in img) and "screenshot" in name.lower()
+
+def is_document(file):
+    return os.path.splitext(file)[1] in doc
+
+def is_data(file):
+    return os.path.splitext(file)[1] in 
+
 input_path = input("Enter files location: ")
 
 
@@ -8,7 +50,11 @@ if not input_path:
     curent_user = os.environ.get('USER') or os.environ.get('LOGNAME')
     change_to_dir = f"/home/{curent_user}/Downloads"
     os.chdir(change_to_dir)
-    print(os.getcwd())    
+    #print(os.getcwd())
+
 
 else:
-    print(input_path)
+    try:
+        os.path.isdir(input_path)
+    except OSError as e:
+        print("Error")
